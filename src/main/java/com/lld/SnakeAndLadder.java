5fbd282d4f853923beb9980aa2e90128674d7efe
mukundplw <mukund.paliwal@rivigo.com>
2024-07-13 17:43:57 +0530

    First Commit

--- src/main/java/com/lld/SnakeAndLadder.java
+++ src/main/java/com/lld/SnakeAndLadder.java
@@ -1,32 +1,103 @@
 package com.lld;
 
+import java.util.ArrayDeque;
+import java.util.Collections;
+import java.util.Deque;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Random;
+import lombok.Getter;
+import lombok.Setter;
 
 public class SnakeAndLadder {
 
 
     public class Game {
-        private Board board;
+        private final Board board;
 
-        private Dice dice;
+        private final Dice dice;
 
-        private Map<Player, Integer> playersPositionMap;
+        private GameStatus gameStatus;
 
-        private LinkedList<Player> playerTurns;
+        private final LinkedList<Player> playerRankingsList = new LinkedList<>();
 
-        public void makeMove() {}
+        private final Deque<Player> playerTurns;
+
+        public Game(Board board, Dice dice, List<Player> players) {
+            this.board = board;
+            this.dice = dice;
+            this.playerTurns = new ArrayDeque<>(players);
+            this.gameStatus = GameStatus.ONGOING;
+        }
+
+        public void makeMove() {
+            if (GameStatus.FINISHED.equals(this.gameStatus)) {
+                throw new RuntimeException("Game already finished");
+            }
+            Player currentPlayer = this.playerTurns.poll();
+            if (currentPlayer.getCurrentPosition() >= this.board.getTotalCells()) {
+                throw new RuntimeException("Player already reached the end");
+            }
+            int score = this.dice.rollDice();
+            int nextPosition = this.board.getNextPosition(score, currentPlayer.getCurrentPosition());
+            updatePlayersAndCheckGameStatus(nextPosition, currentPlayer);
+        }
+
+        private void updatePlayersAndCheckGameStatus(int nextPosition, Player currentPlayer) {
+            currentPlayer.setCurrentPosition(nextPosition);
+            if (nextPosition == this.board.getTotalCells()) {
+                this.playerRankingsList.addLast(currentPlayer);
+            } else {
+                this.playerTurns.addLast(currentPlayer);
+            }
+            if (this.playerTurns.size() == 1) {
+                this.playerRankingsList.addLast(this.playerTurns.poll());
+            }
+            if (this.playerTurns.isEmpty()) {
+                this.gameStatus = GameStatus.FINISHED;
+            }
+        }
+
+        public Player getNextPlayerTurn() {
+            if (GameStatus.FINISHED.equals(this.gameStatus)) {
+                throw new RuntimeException("Game already finished");
+            }
+            return this.playerTurns.peek();
+        }
 
     }
 
+    @Getter
+    @Setter
     public class Board {
 
+        /* Could add List of Cells -> Cells will have 3 implementation -> Normal, Snake & Ladder Cell */
         private Integer totalCells;
 
         private Map<Integer, Integer> snakeStartEndMap;
 
         private Map<Integer, Integer> ladderStartEndMap;
+
+        public Board(Integer totalCells, Map<Integer, Integer> snakeStartEndMap, Map<Integer, Integer> ladderStartEndMap) {
+            this.totalCells = totalCells;
+            this.snakeStartEndMap = snakeStartEndMap;
+            this.ladderStartEndMap = ladderStartEndMap;
+        }
+
+        public Board(Integer totalCells) {
+            this(totalCells, Collections.emptyMap(), Collections.emptyMap());
+        }
+
+        public int getNextPosition(int score, int currentPosition) {
+            int nextPosition = score + currentPosition;
+            if (snakeStartEndMap.get(nextPosition) != null) {
+                nextPosition = snakeStartEndMap.get(nextPosition);
+            } else if (ladderStartEndMap.get(nextPosition) != null) {
+                nextPosition = ladderStartEndMap.get(nextPosition);
+            }
+            return nextPosition;
+        }
 
     }
 
@@ -37,21 +108,24 @@ public class SnakeAndLadder {
         private final Integer DICE_SIZE = 6;
 
         public Integer rollDice() {
-            return random.nextInt(DICE_SIZE + 1);
+            return 1 + random.nextInt(DICE_SIZE);
         }
 
     }
 
+    @Getter
+    @Setter
     public class Player {
 
         private String aliasName;
 
+        private Integer currentPosition;
+
     }
 
-    public enum CellType {
-        REGULAR,
-        SNAKE,
-        LADDER
+    public enum GameStatus {
+        ONGOING,
+        FINISHED
     }
 
 }
